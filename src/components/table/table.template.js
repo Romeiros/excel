7bdb887f3942376121468aba00f833caed35c98b
Romeiros <romeiro2689@gmail.com>
2020-06-25 20:59:14 +0300

    finish framework

---
+++ src/components/table/table.template.js
@@ -0,0 +1,55 @@
+
+const CODES = {
+  A: 65,
+  Z: 90,
+};
+
+function toCell() {
+  return `
+    <div class="cell"></div>
+  `;
+}
+
+function toColumn(col) {
+  return `
+    <div class="column">${col}</div>
+  `;
+}
+
+function createRow(index, content) {
+  return `
+    <div class="row">
+      <div class="row-info">${index ? index : ''}</div>
+      <div class="row-data">${content}</div>
+    </div>
+  `;
+}
+
+function toChar(_, index) {
+  return String.fromCharCode(CODES.A + index + 1);
+}
+
+export function createTable(rowsCount = 15) {
+  const colsCount = CODES.Z - CODES.A;
+  const rows = [];
+
+  const cols = new Array(colsCount)
+      .fill('')
+      .map(toChar)
+      .map(toColumn)
+      .join('');
+
+  rows.push(createRow(null, cols));
+
+  for (let i = 0; i < rowsCount; i++) {
+    const cells = new Array(colsCount)
+        .fill('')
+        .map(toCell)
+        .join('');
+    rows.push(createRow(i + 1, cells));
+  }
+
+  console.log(rows);
+
+  return rows.join('');
+}
